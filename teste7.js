501438f53ec683f4328e5d175ba5cdf31abf79db
harold <s.lefebvre907@laposte.net>
2020-11-04 16:04:33 +0100

    exercice javascript

--- teste7.js
+++ teste7.js
@@ -1,13 +1,20 @@
 
-
-  //var str1 ="robert dupont amiens 80000";
- 
-  var str2 = ";";
-  var  str1 = "";
+function strtok(str1,str2,n){
+    //on recupére entre le délimiteur et la valeur de n
+        var mot1 = str1.split(str2,n);
+        //on retire 1 a la valeur de n a cause du zero
+    var selectnbr = n -1
+    //on recupére le mot numero selectnbr 
+      var retourmot = mot1[selectnbr]
+    //on affiche le numéro choisi et le mot séléctionné
+     document.write("le mot N°"+n+"  est " +retourmot)
+    }
+    
+  var str2 = ";";//délimiteur
+  var  str1 = "";//on déclare
   var numero = 1;
-  var motentrer = true
- 
-    while(motentrer) {
+  var motentrer = true//on lui attribue true pour commencer
+    while(motentrer) {//boucle jusqu'a temps que motentrer soit à false
     var motselect  = window.prompt("Entrer le mot n°"+numero);
         if (motselect != null&&motselect != "") {
              str1 += motselect+";"
@@ -17,18 +24,7 @@
     break
      }
      }
-     n  = window.prompt("entré un chiffre");
-     console.log("liste des mot"+str1)
+     // on s'assure que la valeur de n soit lu en tant que entier
+     n  = parseInt(window.prompt("entré un chiffre"));
+     //appel de la fonction str1 les mot str2 le délimiteur n le numéro du mot séléctionné
   strtok(str1,str2,n);
-    
-
-
-function strtok(str1,str2,n){
- var nf = parseInt(n)
-    var mot1 = str1.split(str2,nf);
-var selectnbr = nf -1
-  var retourmot = mot1[selectnbr]
-    
-    console.log("retour fonction mot 1 "+mot1+" suite "+mot1[0][nf],retourmot,n)
-    document.write("le "+nf+" mot est " +retourmot)
-}
