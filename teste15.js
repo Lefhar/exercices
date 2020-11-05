cc2e1bf4257b13e7643d052c64401db5f20910d8
Lefhar <s.lefebvre907@laposte.net>
2020-11-05 16:52:09 +0100

    exercices de javascript

--- teste15.js
+++ teste15.js
@@ -1,10 +1,29 @@
-function GetInteger(n){
-    return parseInt(n);
+function GetInteger(){
+integer = parseInt(window.prompt("entrer un nombre de d'élément"));
+  
     }
 
-function InitTab(it){
-        return    it = new Array(GetInteger(it));
+function InitTab(integer){
+           itab = new Array(integer); // création du tableau
+           itab.splice(0, integer);       
         }
-function SaisieTab(){
-    
+function SaisieTab(integer,itab){
+    var n = integer
+    console.log(integer)
+    var tableau = itab
+    for(var i=0; i<n;i++){
+         tableau[i] = window.prompt("entrer le mot"+i);
+     
+
+    }
 }
+    function AfficheTab(tableau){
+        var table = tableau;
+        document.write(table)
+    }
+
+
+GetInteger();
+ InitTab(integer);
+SaisieTab(integer,itab);
+AfficheTab(table)
