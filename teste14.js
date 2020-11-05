95baec40afc61cd264c563cde8d8fdd76a2e33ea
Lefhar <s.lefebvre907@laposte.net>
2020-11-05 11:51:01 +0100

    exercices de javascript

---
+++ teste14.js
@@ -0,0 +1,7 @@
+var nbtb = parseInt(window.prompt("entrer le nombre de d'élément"));
+var tableau = new Array(nbtb);
+console.log(nbtb)
+for(i=0; i<nbtb; i++){
+     tableau[i] = window.prompt(" entrer vos "+nbtb+"élément");
+}
+document.write(i+"élément du tableau"+tableau+" teste" );
