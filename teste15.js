b5726701af69167349d5cc284ffe08b4887d5b17
harold <s.lefebvre907@laposte.net>
2020-11-06 15:41:18 +0100

    exercice javascript

--- teste15.js
+++ teste15.js
@@ -8,41 +8,44 @@ function InitTab(integer){
            itab.splice(0, integer);       
         }
 function SaisieTab(integer,itab){//saisi des éléments
-    var content = '';
+    var saitab = '';
     for(var i=0; i<integer;i++){
         // on entre dans le tableau les éléments jusqu'a temps qu'on arrive au nombre inscrit juste avant
-        content = window.prompt('indiquer une valeur à entrer dans le tableau :');
-        var arrayPush = itab.push(content);
-        console.log(arrayPush);
+        saitab = window.prompt('indiquer une valeur à entrer dans le tableau :');
+        var table = itab.push(saitab);
+        console.log(table);
     }
    
 }
-    function AfficheTab(itab){//on affiche le tableau
+    function AfficheTab(){//on affiche le tableau
         console.log(itab);
         alert(itab.join(', '));
-     
  }
 
-   function RechercheTab(select){
-    select = parseInt(window.prompt(`Afficher un index choisi`));
-    var retab = table[select]
-
+   function RechercheTab(){
+    var select = parseInt(window.prompt(`Afficher un index choisi`));
+    var retab = itab[select];
+    alert(itab[select])
    } 
-   function InfoTab(table){
+   function InfoTab(){
+    console.log(Math.max(...itab));
        //le tableau début a 0
-       var min = 0
+       var debut= 0;
        // on recupére la valeur maximum du tableau
-       var max = table.length
-       // on additionne le minimum et le maximum d'élément
-      var resultat = parseInt(min)+parseInt(max)
+       var max = Math.max(...itab);
+       for(var i=0; i<itab.length;i++){
+        debut += parseInt(itab[i])   
+       }
+       console.log("valeur debut"+debut)
       //ensuite on calcule la moyenne du tableau 
-      var moyenne = resultat/max;
+      var moyenne = debut/itab.length;
       // on affiche notre moyenne d'élément
-      document.write(" et la moyenne du tableau "+moyenne)
+      alert("la valeur maximum est "+max+" et la moyenne du tableau "+moyenne)
    }
 GetInteger();
  InitTab(integer);
 SaisieTab(integer, itab);
+
 var choix= parseInt(window.prompt("choisissez une option -1 afficher le tableau -2 afficher un index choisi -3 afficher le max et la moyenne des postes entrer"))
 if(choix==1){
     AfficheTab();  
@@ -56,4 +59,3 @@ if(choix==1){
     InfoTab();
     var choix= parseInt(window.prompt("choisissez une option -1 afficher le tableau -2 afficher un index choisi -3 afficher le max et la moyenne des postes entrer"))
 }
-
