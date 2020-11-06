a5b7496d16f74802ceb425a894be95c39b0302c6
Lefhar <s.lefebvre907@laposte.net>
2020-11-06 10:48:25 +0100

    exercice javascript

--- teste15.js
+++ teste15.js
@@ -7,23 +7,39 @@ function InitTab(integer){
            itab = new Array(integer); // création du tableau
            itab.splice(0, integer);       
         }
-function SaisieTab(integer,itab){
+function SaisieTab(integer,itab){//saisi des éléments
     var n = integer
-    console.log(integer)
     var tableau = itab
     for(var i=0; i<n;i++){
+        // on entre dans le tableau les éléments jusqu'a temps qu'on arrive au nombre inscrit juste avant
          tableau[i] = window.prompt("entrer le mot"+i);
-     
-
     }
+    table =  tableau;
 }
-    function AfficheTab(tableau){
-        var table = tableau;
-        document.write(table)
-    }
+    function AfficheTab(table){//on affiche le tableau
+        //on demande de séléctionner un élément du tableau
+  select = parseInt(window.prompt(`voici vos mots ${table}  entrer le Numéro  que vous souhaitez afficher`));
+        }
 
-
+   function RechercheTab(select){
+      var mot = table[select]
+document.write("le mot choisi c'est "+mot)
+   } 
+   function InfoTab(table){
+       //le tableau début a 0
+       var min = 0
+       // on recupére la valeur maximum du tableau
+       var max = table.length
+       // on additionne le minimum et le maximum d'élément
+      var resultat = parseInt(min)+parseInt(max)
+      //ensuite on calcule la moyenne du tableau 
+      var moyenne = resultat/max;
+      // on affiche notre moyenne d'élément
+      document.write(" et la moyenne du tableau "+moyenne)
+   }
 GetInteger();
  InitTab(integer);
 SaisieTab(integer,itab);
-AfficheTab(table)
+AfficheTab(table);
+RechercheTab(select);
+InfoTab(table);
