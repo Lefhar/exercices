abd8084c1dde29d47b12e11da5e4b00b7838ac97
Lefhar <s.lefebvre907@laposte.net>
2020-11-10 10:01:09 +0100

    exercices javascript

--- teste21.js
+++ teste21.js
@@ -5,41 +5,58 @@ function verif(){
 var ville = document.formulaire.ville.value;
 var tel = document.formulaire.tel.value;
 var email = document.formulaire.email.value;
-var verif 	= /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,3}$/
+var verif 	= /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]{2,}[.][a-zA-Z]{2,4}$/
+var ctrsociete = /^[a-zA-Z]{2,}$/
+var ctrpcontact = /^[a-zA-Z]{2,}$/
+var ctrville = /^[a-zA-Z]{2,}$/
+var ctrcp 	= /^[0-9]{5,}$/
+var ctrtel 	= /^[0-9]{10,}$/
 if (verif.exec(email) == null){
     console.log(email)
     document.getElementById("demail").innerHTML="<div class=\"alert alert-danger\" role=\"alert\">vous devez entrer une email correct</div>";
-    var controlmail = true;
+    document.getElementById("email").className = "form-control is-invalid";
+    var controlmail = false;
 }else{
+    document.getElementById("email").className = "form-control is-valid";
  var controlmail = true;
 } 
-if(cp.length < 5 ){
+if (ctrcp.exec(cp) == null){
     document.getElementById("dcp").innerHTML="<div class=\"alert alert-danger\" role=\"alert\">vous devez entrer 5 chiffres</div>";
-    var controlcp = true;
+    var controlcp = false;
+    document.getElementById("cp").className = "form-control is-invalid";
 }else{
+    document.getElementById("cp").className = "form-control is-valid";
  var controlcp = true;
 }
-if(pcontact.length < 1 ){
+if (ctrpcontact.exec(pcontact) == null){
     document.getElementById("dpcontact").innerHTML="<div class=\"alert alert-danger\" role=\"alert\">Entrez le nom d'une personne à contacter</div>";
     var controlpcontact = false;
+    document.getElementById("pcontact").className = "form-control is-invalid";
 }else{
+    document.getElementById("pcontact").className = "form-control is-valid";
     var controlpcontact = true;
 }
-if(societe.length < 1 ){
+if (ctrsociete.exec(societe) == null){
     document.getElementById("dsociete").innerHTML="<div class=\"alert alert-danger\" role=\"alert\">Entrez le nom de la société</div>";
     var controlsociete = false;
+    document.getElementById("societe").className = "form-control is-invalid";
 }else{
+    document.getElementById("societe").className = "form-control is-valid";
     var controlsociete = true;
 }
-if(ville.length < 1 ){
+ if (ctrville.exec(ville) == null){
     document.getElementById("dville").innerHTML="<div class=\"alert alert-danger\" role=\"alert\">Entrez le nom de votre ville</div>";
     var controlville = false;
+    document.getElementById("ville").className = "form-control is-invalid";
 }else{
+    document.getElementById("ville").className = "form-control is-valid";
     var controlville = true;
 }
-if(tel.length < 1 ){
+if (ctrtel.exec(tel) == null){
     document.getElementById("dtel").innerHTML="<div class=\"alert alert-danger\" role=\"alert\">Entrez un numéro de téléphone correct</div>";
+    document.getElementById("tel").className = "form-control is-invalid";
 }else{
+    document.getElementById("tel").className = "form-control is-valid";
     var controltel = true;
 }
 if(controlcp == true  &&controlmail == true&&controlpcontact == true &&controlcp == true &&controlsociete == true &&controltel == true){
