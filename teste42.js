fac20015c5da60b4bc0218e9234d449fb8b28455
Lefhar <s.lefebvre907@laposte.net>
2020-11-12 16:35:20 +0100

    exercice javascript

---
+++ teste42.js
@@ -0,0 +1,6 @@
+var age = parseInt(window.prompt("entrer votre âge"))
+var durpermis = parseInt(window.prompt("depuis combien de temps avez vous le permis ?"))
+var accident = parseInt(window.prompt("entrer le nombre d'accident que vous avez déjà eu"))
+if(accident ==true){
+    var result= "vous être refusé"
+}else if()
