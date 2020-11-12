d694f6923a20a6ea40d65fa9c53587837a142b5a
Lefhar <s.lefebvre907@laposte.net>
2020-11-12 16:51:56 +0100

    exercice javascript

--- teste42.js
+++ teste42.js
@@ -1,6 +1,11 @@
+//https://ncode.amorce.org/ressources/Pool/TB/Accueil_Ecrire_algorithme_1/Exercices4.pdf
 var age = parseInt(window.prompt("entrer votre âge"))
 var durpermis = parseInt(window.prompt("depuis combien de temps avez vous le permis ?"))
 var accident = parseInt(window.prompt("entrer le nombre d'accident que vous avez déjà eu"))
 if(accident ==true){
     var result= "vous être refusé"
-}else if()
+}else if(age <25&&durpermis<2&&accident==0){
+var result = "tarif rouge"
+}else if(age<25&&durpermis>2&&accident==0){
+    var result = "tarif orange"
+}else if(age <25&&durpermis>2&&accident==0){
